6d691fb2a7ea31b2752d1926fa323cf6e689b9a3
divshah <divshah@paypal.com>
2018-10-23 23:44:38 -0700

    WIP - React router with redux

--- src/index.js
+++ src/index.js
@@ -6,10 +6,32 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise'
+
+import PostsIndex from './components/post_index'
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+// class Hello extends React.Component {
+//   render(){
+//     return <div>Hello!</div>
+//   }
+// }
+
+
+// class Goodbye extends React.Component {
+//   render(){
+//     return <div>Goodbye!</div>
+//   }
+// }
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex}></Route>
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
